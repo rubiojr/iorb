ef8879d8f78c6a6f593f839659ee86e1f2db86e2
Sergio Rubio <sergio@rubio.name>
2009-05-04 11:35:44 +0200

    * bumped to version 0.5.2

--- lib/iorb.rb
+++ lib/iorb.rb
@@ -15,7 +15,7 @@ class Dropio::Asset
   end
 end
 module IORB
-  VERSION = "0.5.1"
+  VERSION = "0.5.2"
   module Util
     # Code stolen from:
     # http://evan.tiggerpalace.com/2008/04/26/pastie-from-the-mac-clipboard/
